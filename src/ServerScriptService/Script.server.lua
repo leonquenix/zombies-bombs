439729595f2de1458c7404f3ec9af9e2d74d8f7d
leonquenix <107597542+leonquenix@users.noreply.github.com>
2022-06-21 15:42:18 -0300

    Removing missing debug code

--- src/ServerScriptService/Script.server.lua
+++
@@ -1,4 +0,0 @@
--- Adicionar um novo atributo no jogador que controla a quantidade de bombas dropadas ao mesmo tempo
-
--- Corrigir o bug do POWER não estar aplicado durante a sessão
--- (está funcionando somente quando o jogador sai e entra de novo)
